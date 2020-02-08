88e579e7f1963de46a7012016eae5a85783282fe
Animesh Kumar <animesh@Animeshs-MacBook-Pro-Touch-3.local>
2020-02-08 22:53:38 +0530

    added pre-demo-ui

---
+++ pre-demo-web/precache-manifest.5d950fce6c991be3f545384202bc5e92.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a134b51945494d8f5923ea2461c2982",
+    "url": "/index.html"
+  },
+  {
+    "revision": "011b86202882919e23c0",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "3fe75b678aa61c6e4309",
+    "url": "/static/css/main.466e8ad9.chunk.css"
+  },
+  {
+    "revision": "011b86202882919e23c0",
+    "url": "/static/js/2.ada0be28.chunk.js"
+  },
+  {
+    "revision": "8b0854dbace574fdd9bdaf468e523a50",
+    "url": "/static/js/2.ada0be28.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3fe75b678aa61c6e4309",
+    "url": "/static/js/main.f9195459.chunk.js"
+  },
+  {
+    "revision": "16e032be1594885c2a0f",
+    "url": "/static/js/runtime-main.400aa179.js"
+  },
+  {
+    "revision": "95e76fcdd5efac45cb1cf7733638dd94",
+    "url": "/static/media/Google-Analytics-Logo-PNG.95e76fcd.png"
+  },
+  {
+    "revision": "4667677d63051003c9c24a06ced6706a",
+    "url": "/static/media/alert.4667677d.png"
+  },
+  {
+    "revision": "4868f1079446f92d62da0f5f0f7d4b6f",
+    "url": "/static/media/customer-profile.4868f107.png"
+  },
+  {
+    "revision": "5d6ea38a769498dfc19fe6389d14db39",
+    "url": "/static/media/fb.5d6ea38a.png"
+  },
+  {
+    "revision": "42154008e5d63e4404b74587fb1e972b",
+    "url": "/static/media/omniture.42154008.png"
+  },
+  {
+    "revision": "e6709f2f7563d70b2679c1b16eb4f683",
+    "url": "/static/media/sales.e6709f2f.png"
+  },
+  {
+    "revision": "0ce139038139bde03a0256d8fab2292c",
+    "url": "/static/media/salesforce.0ce13903.png"
+  }
+]);
